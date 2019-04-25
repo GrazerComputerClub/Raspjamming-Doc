0fe8739c03704d0351d54bc4e2c8fcdd8bed4558
Manfred Wallner <mwallner@users.noreply.github.com>
2019-04-25 18:11:12 +0200

    Delete .build_win.ps1

--- .build_win.ps1
+++
@@ -1,9 +0,0 @@
-version: 1.0.{build}
-image: Visual Studio 2017
-install:
-  - ps: choco install -y miktex
-  - cmd:  refreshenv
-
-build_script:
-  - cmd: pdflatex .\Raspjamming.tex
-  - cmd: pdflatex .\Raspjamming - Admin.tex
